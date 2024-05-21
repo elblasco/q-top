4fa8709c8f9537161907e9264909dddb010c9654
elblasco <64553614+elblasco@users.noreply.github.com>
2024-05-21 11:08:13 +0200

    add: main class skeleton

---
+++ src/main/java/it/unitn/disi/ds1/qtop/QTop.java
@@ -0,0 +1,45 @@
+package it.unitn.disi.ds1.qtop;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import it.unitn.disi.ds1.qtop.Utils.StartMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static it.unitn.disi.ds1.qtop.Utils.N_NODES;
+
+public class QTop{
+
+    public static void main(String[] args) {
+
+        // Create the actor system
+        final ActorSystem system = ActorSystem.create("qtop");
+
+        // Create a "virtual synchrony manager"
+        ActorRef coordinator = system.actorOf(Node.props(-1), "coordinator");
+
+        // Create nodes and put them to a list
+        List<ActorRef> group = new ArrayList<>();
+        for (int i = 0; i < N_NODES; i++) {
+            group.add(system.actorOf(Node.props(i), "node" + i));
+        }
+
+        // Send start messages to the participants to inform them of the group
+        StartMessage start = new StartMessage(group);
+        for (ActorRef peer: group) {
+            peer.tell(start, null);
+        }
+
+        coordinator.tell(start, null);
+
+        try {
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        }
+        catch (IOException ignored) {}
+        system.terminate();
+
+    }
+}
