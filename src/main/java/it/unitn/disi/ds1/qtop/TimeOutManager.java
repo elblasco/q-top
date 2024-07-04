209a8c9961debd1a69b9f8fbde78eb3489306196
elblasco <64553614+elblasco@users.noreply.github.com>
2024-07-04 16:27:35 +0200

    fix: countdown and timer reset

--- src/main/java/it/unitn/disi/ds1/qtop/TimeOutManager.java
+++ src/main/java/it/unitn/disi/ds1/qtop/TimeOutManager.java
@@ -8,27 +8,71 @@ import java.util.EnumMap;
 
 public class TimeOutManager extends EnumMap<Utils.TimeOutReason, ArrayList<Pair<Cancellable, Integer>>> {
 	// Phony map to associate a reason with its specific refresh ratio
-	private final EnumMap<Utils.TimeOutReason, Integer> timeoutRefresh;
+	private final EnumMap<Utils.TimeOutReason, Integer> customTimeouts;
 	private final int refresh;
 
 	public TimeOutManager(int decisionTimeout, int voteTimeout,
 			int heartbeatTimeout, int refresh) {
 		super(Utils.TimeOutReason.class);
-		this.timeoutRefresh = new EnumMap<>(Utils.TimeOutReason.class);
-		timeoutRefresh.put(Utils.TimeOutReason.VOTE, voteTimeout / refresh);
-		timeoutRefresh.put(Utils.TimeOutReason.HEARTBEAT, heartbeatTimeout / refresh);
-		timeoutRefresh.put(Utils.TimeOutReason.DECISION, decisionTimeout / refresh);
+		this.customTimeouts = new EnumMap<>(Utils.TimeOutReason.class);
+		customTimeouts.put(
+				Utils.TimeOutReason.VOTE,
+				voteTimeout
+		);
+		customTimeouts.put(
+				Utils.TimeOutReason.HEARTBEAT,
+				heartbeatTimeout
+		);
+		customTimeouts.put(
+				Utils.TimeOutReason.DECISION,
+				decisionTimeout
+		);
 		this.refresh = refresh;
+		for (Utils.TimeOutReason reason : Utils.TimeOutReason.values())
+		{
+			this.put(
+					reason,
+					new ArrayList<>()
+			);
+		}
 	}
 
 	public void startCountDown(Utils.TimeOutReason reason, Cancellable action, int i) {
-		if (this.get(reason) == null || this.get(reason).size() < i) {
+		if (this.get(reason).size() < i)
+		{
 			int initialSize = this.get(reason).size();
 			for (int x = 0; x < (i - initialSize); x++) {
-				this.get(reason).add(new Pair<>(null, this.timeoutRefresh.get(reason)));
+				// Padding of empty pairs
+				this.get(reason).add(new Pair<>(
+						null,
+						this.customTimeouts.get(reason)
+				));
 			}
 		}
-		this.get(reason).add(new Pair<>(action, this.timeoutRefresh.get(reason)));
+		this.get(reason).add(new Pair<>(
+				action,
+				this.customTimeouts.get(reason)
+		));
+	}
+
+	public void resetCountDown(Utils.TimeOutReason reason, int i, int nodeId, Logger logger) {
+		logger.log(
+				Utils.LogLevel.INFO,
+				"[NODE-" + nodeId + "] is reseting " + i + "th countdown for " + reason
+		);
+		switch (reason)
+		{
+			case HEARTBEAT: // in the HEARTBEAT case the timer is reset to its max
+				this.get(reason).set(
+						i,
+						new Pair<>(
+								this.get(reason).get(i).first(),
+								this.customTimeouts.get(reason)
+						)
+				);
+			default: // in all other cases the timeout countdown is cancelled
+				this.get(reason).get(i).first().cancel();
+		}
 	}
 
 	public void handleCountDown(Utils.TimeOutReason reason, int i, int nodeId, Logger logger){
@@ -37,17 +81,23 @@ public class TimeOutManager extends EnumMap<Utils.TimeOutReason, ArrayList<Pair<
 			this.get(reason).get(i).first().cancel();
 			logger.log(
 					Utils.LogLevel.INFO,
-					"[NODE-" + nodeId + "] coordinator HEARTBEAT timeout"
+					"[NODE-" + nodeId + "] " + reason + " timeout"
 			);
 		}
 		else
 		{
+			this.get(reason).set(
+					i,
+					new Pair<>(
+							this.get(reason).get(i).first(),
+							this.get(reason).get(i).second() - (this.customTimeouts.get(reason) / this.refresh)
+					)
+			);
 			logger.log(
 					Utils.LogLevel.DEBUG,
-					"[NODE-" + nodeId + "] received HEARTBEAT from coordinator"
+					"[NODE-" + nodeId + "] has not received " + reason + " yet, " + this.get(reason).get(i)
+							.second() + " seconds left"
 			);
-			this.get(reason).set(i, new Pair<>(this.get(reason).get(i).first(),
-					this.get(reason).get(i).second() - this.timeoutRefresh.get(reason)));
 		}
 	}
 }
