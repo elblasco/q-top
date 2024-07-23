6c461f815a9766b30127bf5c0fc0683387c9243e
elblasco <64553614+elblasco@users.noreply.github.com>
2024-07-23 15:58:35 +0200

    del: unused imports

--- src/main/java/it/unitn/disi/ds1/qtop/QTop.java
+++ src/main/java/it/unitn/disi/ds1/qtop/QTop.java
@@ -1,14 +1,5 @@
 package it.unitn.disi.ds1.qtop;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import it.unitn.disi.ds1.qtop.Utils.StartMessage;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class QTop{
 
     public static void main(String[] args) {
